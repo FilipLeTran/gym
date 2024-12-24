2de7fcc605bc08bd19f2d5245a6adcc03ae560c2
filipletran <filipletran@gmail.com>
2024-12-24 21:03:39 +0100

    add initial empty structure and commments on feature

---
+++ apps/gym-logger-client/src/app/components/Program/Exercise/index.tsx
@@ -0,0 +1,8 @@
+// This component is supposed to show the exercie in more detail.
+// In here it will show the rest time as well as the reps and sets.
+// Furthremore, it will also show the alterantives to the exercise and
+// where I can perform them (like cable or machine or barbell and so on)
+// Unrelated to this component is that in here it will also allow the user
+// to log their number of reps and for how long they rested, a future
+// functionality could be to automatically log how long rest a person took
+// when adding an exercise or the user themselves press a "go" button
---
+++ apps/gym-logger-client/src/app/components/Program/index.tsx
@@ -0,0 +1,8 @@
+// The program component is supposed to act
+// as the list of all the exercies in the workout program
+// Its sole function is to do nothing but list the exercises
+// and in which order they are supposed to perform
+// and which one has been completed.
+// A future feature that might be added is alternative
+// exercises that can be used instead as well and also
+// the number of sets and reps for each exercise
